cc9a217526cb83a463f12fbf740139843a6e3371
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-07-03 20:55:33 +0530

    ADDED: According to message returns sad or happy mood

---
+++ test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+    private MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+
+
+    @Test
+    public void givenMessage_WhenSad_ShouldReturnSad() {
+        String mood = moodAnalyzer.analyzeMood("I am in sad mood");
+        Assert.assertEquals("SAD", mood);
+    }
+
+    @Test
+    public void givenMessage_WhenNotSad_ShouldReturnHappy() {
+        String mood = moodAnalyzer.analyzeMood("I am in any mood");
+        Assert.assertEquals("HAPPY", mood);
+    }
+}
